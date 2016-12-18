72e0162cae7676e922dac66b379e45ab3084477d
Mihawk <victra.sonata@gmail.com>
2016-12-18 23:10:52 +0700

    done image processing

---
+++ tutor9(convert)/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(260, 197)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"JPEG", "PSD", "PNG", "GIF", "BMP"})
+        Me.ComboBox1.Location = New System.Drawing.Point(132, 228)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(140, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 228)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Select"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(72, 228)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Convert"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ tutor9(convert)/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "Masukkan Gambar"
+        OpenFileDialog1.Filter = "image file (*.jpg) |*.jpg| image file (*.psd) |*.psd| image file (*.png) |*.png| image file (*.gif) |*.gif| image file (*.bmp) |*.bmp| all files (*.*) | *.*"
+        OpenFileDialog1.FileName = ""
+        If OpenFileDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ComboBox1.SelectedItem = "JPEG" Then
+            Try
+                SaveFileDialog1.Filter = "JPEG |*.jpeg"
+                If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                End If
+            Catch ex As Exception
+            End Try
+        End If
+        If ComboBox1.SelectedItem = "PSD" Then
+            Try
+                SaveFileDialog1.Filter = "PSD |*.psd"
+                If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                End If
+            Catch ex As Exception
+            End Try
+        End If
+        If ComboBox1.SelectedItem = "PNG" Then
+            Try
+                SaveFileDialog1.Filter = "PNG |*.png"
+                If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                End If
+            Catch ex As Exception
+            End Try
+        End If
+        If ComboBox1.SelectedItem = "GIF" Then
+            Try
+                SaveFileDialog1.Filter = "GIF |*.gif"
+                If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                End If
+            Catch ex As Exception
+            End Try
+        End If
+        If ComboBox1.SelectedItem = "BMP" Then
+            Try
+                SaveFileDialog1.Filter = "BMP |*.bmp"
+                If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg)
+                End If
+            Catch ex As Exception
+            End Try
+        End If
+        MsgBox("Success")
+    End Sub
+
+    Private Sub Form1_load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedItem = "JPEG"
+    End Sub
+End Class
